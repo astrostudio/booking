1c92b623a7145fb7d84388a81817811426f589ef
Maciej Lison <maciej.lison@ai.pwn.pl>
2023-01-23 18:36:41 +0100

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 composer install
-php artisan sail:install --with=2
+php artisan sail:install --with=mariadb
 ./vendor/bin/sail up -d
 ./vendor/bin/sail artisan migate:install
 ./vendor/bin/sail artisan migrate
