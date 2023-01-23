1d7334d95ba735f12584fe9114eab66906a07c69
Maciej Lison <maciej.lison@ai.pwn.pl>
2023-01-23 18:40:53 +0100

    build

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@ composer install
 cp .env.example .env
 php artisan sail:install --with=mariadb
 ./vendor/bin/sail up -d
-./vendor/bin/sail artisan migate:install
+./vendor/bin/sail artisan migrate:install
 ./vendor/bin/sail artisan migrate
 ./vendor/bin/sail artisan db:seed --class=RoomTableSeeder
